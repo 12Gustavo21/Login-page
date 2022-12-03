0a68bdf4a339a9d373e0e3f913ba52a018e51f54
12Gustavo21 <gustavo927318@gmail.com>
2022-12-03 02:01:53 -0300

    Login and SignUp Page without responsive

---
+++ scripts/SignUp.js
@@ -0,0 +1,12 @@
+Particles.init({
+    selector: '.SignUpBackground',
+    color: ['#1cefff'],
+    connectParticles: true,
+    responsive: [{
+        breakpoint: 800,
+        options: {
+            maxParticles: 300,
+            connectParticles: false
+        }
+    }]
+});
